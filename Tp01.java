378674be518a6b4597cd318ff7c45ff3b4b9613d
walmirifsp <48810953+walmirifsp@users.noreply.github.com>
2020-09-21 18:34:47 -0300

    Add files via upload

---
+++ Tp01.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp01;
+
+/**
+ *
+ * @author walmir
+ */
+import java.util.Scanner;
+public class Tp01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /* ex 1
+        
+        
+        double b , a , r;
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println(" digite o valor da base do retangulo");
+        b = ler.nextDouble();
+        
+        System.out.println(" digite o valor dA altura ");
+        a = ler.nextDouble();
+        
+        r = a * b;
+        
+        System.out.println("o area do retangulo total é " + r + "²");
+        */
+       /*ex 2
+       
+       
+         double a , t;
+        Scanner ler = new Scanner(System.in);
+        System.out.println(" digite o valor da aresta do quadrado");
+        a = ler.nextDouble();
+        
+        t = a * a;
+        System.out.println("o valor total do quadrado  é " + t);
+          */
+       /*ex 4
+       
+       
+       
+            double diag , l;
+            Scanner ler = new Scanner(System.in);
+              System.out.println(" digite a diagonal do quadrado");
+            diag  =   ler.nextDouble();
+            
+            l = Math.pow(2,diag) / 2;
+            System.out.println(" o valor total do quadrado é de " + l);
+
+    */
+    
+    }
+}
